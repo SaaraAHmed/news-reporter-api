295bc34eeb01842468edac860583aac2822fd013
SaaraAHmed <64234071+SaaraAHmed@users.noreply.github.com>
2021-08-24 00:36:22 +0200

    Add files via upload

---
+++ task3 news-reporter-api/src/models/news.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose')
+
+const newsSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:Buffer
+    },
+
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true
+    },
+    avatar:{
+        type:Buffer  
+    }
+
+})
+
+const news = mongoose.model('news',newsSchema)
+module.exports = news
